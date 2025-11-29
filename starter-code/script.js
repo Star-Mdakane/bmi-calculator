edbd4ad5d4c60ff495af74637c790fd502883bcd
Star <mmdakane@rocketmail.com>
2025-11-29 23:29:50 +0200

    calculation of bmi working

--- starter-code/script.js
+++ starter-code/script.js
@@ -27,6 +27,58 @@ const imperialBmi = (weightS = 1, weightP, heightF = 1, heightI) => {
 }
 
 
+const getIdealWeight = (bmi, idealWeightFormula, unitSystem, height) => {
+    const weight = idealWeightFormula(bmi, height);
+
+    return convertWeight(weight, unitSystem);
+}
+
+const convertWeight = (weight, unitSystem) => {
+    if (unitSystem === 'metric') {
+
+        return `${weight.toFixed(2)} kg`;
+    } else if (unitSystem === 'imperial') {
+
+        const stone = Math.floor(weight / 14);
+        const pounds = Math.round(weight % 14);
+        return `${stone} stone ${pounds} pounds`;
+    }
+}
+
+const updateBmiClassifications = (range, height, unitSystem, heightUnit) => {
+
+    let classificationText = '';
+    let idealWeight;
+    let heightInMeters;
+    let heightInInches;
+
+    if (unitSystem === "metric") {
+        if (heightUnit === 'cm') {
+            heightInMeters = height / 100;
+        } else if (heightUnit === 'm') {
+            heightInMeters = height;
+        }
+        idealWeight = (bmi) => bmi * (heightInMeters * heightInMeters);
+    } else if (unitSystem === "imperial") {
+        if (heightUnit === 'ft') {
+            heightInInches = height * 12;
+        } else if (heightUnit === 'in') {
+            heightInInches = height;
+        }
+        idealWeight = (bmi) => (bmi * (heightInInches * heightInInches)) / 703;
+    }
+
+    if (range < 18.5) { classificationText = `<p class="text">Your BMI suggests you're underweight. Your ideal weight is above <span class="text-b">${getIdealWeight(18.5, idealWeight, unitSystem, height)}</span>.</p>` }
+    else if (range >= 18.5 && range < 24.5) { classificationText = `<p class="text">Your BMI suggests you're a healthy weight. Your ideal weight is between <span class="text-b">${getIdealWeight(18.5, idealWeight, unitSystem, height)} - ${getIdealWeight(24.5, idealWeight, unitSystem, height)}</span>.</p>` }
+    else if (range >= 25 && range < 29.5) { classificationText = `<p class="text">Your BMI suggests you're overweight. Your ideal weight is between <span class="text-b">${getIdealWeight(25, idealWeight, unitSystem, height)} - ${getIdealWeight(29.5, idealWeight, unitSystem, height)}</span>.</p>` }
+    else { classificationText = `<p class="text">Your BMI suggests you're obese. Your ideal weight is below <span class="text-b">${getIdealWeight(30, idealWeight, unitSystem, height)}</span>.</p>` }
+
+    return classificationText;
+}
+
+
+
+
 
 metricForm.addEventListener("input", (e) => {
     e.preventDefault();
@@ -46,11 +98,10 @@ metricForm.addEventListener("input", (e) => {
         console.log(height)
         console.log(weight)
         console.log(bmi)
+
+        console.log(updateBmiClassifications(bmi, height, "metric", 'cm'))
     }
 })
-
-
-
 
 imperialForm.addEventListener("input", (e) => {
     e.preventDefault();
@@ -69,7 +120,10 @@ imperialForm.addEventListener("input", (e) => {
         const weightS = Number(imperialForm.querySelector("#weightSt").value);
         const weightP = Number(imperialForm.querySelector("#weightLbs").value);
         const bmi = imperialBmi(weightS, weightP, heightF, heightI);
+        const totalHeightInInches = (heightF * 12) + heightI;
         console.log(bmi)
+        console.log(updateBmiClassifications(bmi, totalHeightInInches, "imperial", 'in'))
+
     }
 })
 
